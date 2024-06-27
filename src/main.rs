6732519a27152b840492862538d85cc91a5bb90b
Colin Huber <cbhuber@gmail.com>
2024-06-26 19:06:56 -0600

    feat: Establish music and API structs

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use reqwest;
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    name: String,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    name: String,
+    artists: Vec<Artist>,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Track {
+    name: String,
+    href: String,
+    popularity: u32,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Items<T> {
+    items: Vec<T>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse {
+    tracks: Items<Track>,
+}
+
+fn main() {
+    println!("");
+}
