0d57046479383a92d496f29ab1806593dd98d75d
Colin Huber <cbhuber@gmail.com>
2024-06-27 19:50:49 -0600

    feat: Main to capture API and track search query

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,37 @@ struct APIResponse {
     tracks: Items<Track>,
 }
 
-fn main() {
-    println!("");
+fn print_tracks(tracks: Vec<&Track>) {}
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let search_query = &args[1];
+    let auth_token = &args[2];
+
+    let url = format!(
+        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
+        query = search_query
+    );
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header(AUTHORIZATION, format!("Bearer {}", auth_token))
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await
+        .unwrap();
+
+    match response.status() {
+        reqwest::StatusCode::OK => match response.json::<APIResponse>().await {
+            Ok(parsed) => print_tracks(parsed.tracks.items.iter().collect()),
+            Err(_) => println!("Error in response."),
+        },
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Please obtain a new token.");
+        }
+        _ => panic!("An unexpected error occurred."),
+    }
 }
